59a793f591b12fa3bfcc60331f11fc87bc67a419
zahid <zahidkhandev@gmail.com>
2024-10-04 21:32:07 +0500

    origin allowed

--- index.js
+++ index.js
@@ -12,12 +12,11 @@ dotenv.config();
 const app = express();
 connectDB();
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-console.log("there");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["POST"],
     credentials: true,
   })
