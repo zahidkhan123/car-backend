760c996ddae3d82f874ba442344ad7b745777109
zahid <zahidkhandev@gmail.com>
2024-10-04 21:03:16 +0500

    url added in db

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(
+      "mongodb+srv://zahidkhandev:Arfa!123@cluster0.5efxp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
     console.log("MongoDB connected");
   } catch (err) {
     console.error(err.message);
